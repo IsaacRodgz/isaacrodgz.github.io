3130b9a91dd846cc286ec87a79c0d2261fa850fb
IsaacRodgz <isaacrodgzb@gmail.com>
2022-11-11 09:49:27 -0600

    Change home path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/isaacrodgzb" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />
