bcd99093b17f7a21a4db9d5cb0d528ed21042af7
Isaac Rodríguez <isaacrodgzb@gmail.com>
2025-08-09 18:01:25 -0600

    Lazy load pages with React.lazy and Suspense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-import Home from './pages/Home';
-import Experience from './pages/Experience';
-import Projects from './pages/Projects';
+import { Suspense, lazy } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import ProjectDisplay from './pages/ProjectDisplay';
 import { Routes, Route, HashRouter } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home'));
+const Experience = lazy(() => import('./pages/Experience'));
+const Projects = lazy(() => import('./pages/Projects'));
+const ProjectDisplay = lazy(() => import('./pages/ProjectDisplay'));
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
-          <Route path="/experience" element={<Experience />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/project/:id" element={<ProjectDisplay />} />
+            <Route path="/experience" element={<Experience />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </HashRouter>
     </div>
